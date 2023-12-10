6267af34dc79458eebaaf819f6e18a8d0bd69328
AnduinXue <anduin@aiursoft.com>
2023-12-10 17:31:12 +0000

    New feature for websocket extends.

---
+++ src/Aiursoft.WebTools/Services/AiurWebSocket.cs
@@ -0,0 +1,89 @@
+using System.Net.WebSockets;
+using System.Text;
+using AiurObserver;
+
+namespace Aiursoft.WebTools.Services;
+
+public class AiurWebSocket : AsyncObservable<string>
+{
+    private bool _dropped;
+    private readonly WebSocket _ws;
+    
+    public bool Connected => !_dropped && _ws.State == WebSocketState.Open;
+    
+    public AiurWebSocket(WebSocket ws)
+    {
+        _ws = ws;
+    }
+    
+    public async Task Send(string message, CancellationToken token)
+    {
+        try
+        {
+            if (_dropped)
+            {
+                throw new WebSocketException("WebSocket is dropped!");
+            }
+            var buffer = new ArraySegment<byte>(Encoding.UTF8.GetBytes(message));
+            await _ws.SendAsync(buffer, WebSocketMessageType.Text, true, token);
+        }
+        catch (WebSocketException)
+        {
+            _dropped = true;
+        }
+    }
+
+    public async Task Listen(CancellationToken token)
+    {
+        try
+        {
+            var buffer = new ArraySegment<byte>(new byte[4 * 1024]);
+            while (true)
+            {
+                var message = await _ws.ReceiveAsync(buffer, token);
+                switch (message.MessageType)
+                {
+                    case WebSocketMessageType.Text:
+                    {
+                        var messageBytes = buffer.Skip(buffer.Offset).Take(message.Count).ToArray();
+                        var messageString = Encoding.UTF8.GetString(messageBytes);
+                        Broadcast(messageString);
+                        break;
+                    }
+                    case WebSocketMessageType.Close:
+                        await _ws.CloseOutputAsync(WebSocketCloseStatus.NormalClosure, "Close because of error.", token);
+                        _dropped = true;
+                        return;
+                    case WebSocketMessageType.Binary:
+                        break;
+                    default:
+                        throw new ArgumentOutOfRangeException();
+                }
+                
+                if (_ws.State == WebSocketState.Open)
+                {
+                    continue;
+                }
+
+                _dropped = true;
+                return;
+            }
+        }
+        catch (WebSocketException)
+        {
+            _dropped = true;
+        }
+    }
+
+    public Task Close(CancellationToken token)
+    {
+        if (_ws.State == WebSocketState.Open)
+        {
+            return _ws.CloseOutputAsync(WebSocketCloseStatus.NormalClosure, "Close because of error.", token);
+        }
+
+        _dropped = true;
+        
+        return Task.CompletedTask;
+    }
+}
---
+++ src/Aiursoft.WebTools/Services/WebSocketExtends.cs
@@ -0,0 +1,23 @@
+﻿using System.Net.WebSockets;
+using Microsoft.AspNetCore.Http;
+
+namespace Aiursoft.WebTools.Services;
+
+public static class WebSocketExtends
+{
+    public static async Task<AiurWebSocket> AcceptWebSocketClient(this HttpContext context)
+    {
+        if (!context.WebSockets.IsWebSocketRequest)
+        {
+            throw new InvalidOperationException("This request is not a WebSocket request!");
+        }
+        return new AiurWebSocket(await context.WebSockets.AcceptWebSocketAsync());
+    }
+    
+    public static async Task<AiurWebSocket> ConnectAsWebSocketServer(this string endpoint)
+    {
+        var client = new ClientWebSocket();
+        await client.ConnectAsync(new Uri(endpoint), CancellationToken.None);
+        return new AiurWebSocket(client);
+    }
+}
