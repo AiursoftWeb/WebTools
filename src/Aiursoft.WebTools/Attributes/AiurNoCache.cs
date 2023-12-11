5468d5e49f13497dec0f8a7923a4c3f118573403
Anduin Xue <xuef@microsoft.com>
2023-12-11 06:39:36 +0000

    Add AiurNoCache.

---
+++ src/Aiursoft.WebTools/Attributes/AiurNoCache.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace Aiursoft.WebTools.Attributes;
+
+public class AiurNoCache : ActionFilterAttribute
+{
+    public override void OnActionExecuting(ActionExecutingContext context)
+    {
+        base.OnActionExecuting(context);
+        if (!context.HttpContext.Response.Headers.ContainsKey("Cache-Control"))
+        {
+            context.HttpContext.Response.Headers.Add("Cache-Control", "no-cache");
+        }
+
+        if (!context.HttpContext.Response.Headers.ContainsKey("Expires"))
+        {
+            context.HttpContext.Response.Headers.Add("Expires", "-1");
+        }
+    }
+}
