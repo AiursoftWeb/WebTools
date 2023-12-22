d9309b4822ddbf56566175cefae19a0a34b74864
AnduinXue <anduin@aiursoft.com>
2023-12-22 07:10:41 +0000

    7.0.19 Better limit permin.

--- src/Aiursoft.WebTools/Attributes/LimitPerMin.cs
+++ src/Aiursoft.WebTools/Attributes/LimitPerMin.cs
@@ -7,9 +7,7 @@ namespace Aiursoft.WebTools.Attributes;
 
 public class LimitPerMin : ActionFilterAttribute
 {
-    private static readonly ConcurrentDictionary<string, int> MemoryDictionary = new();
-    private static DateTime _lastClearTime = DateTime.UtcNow;
-
+    private static readonly ConcurrentDictionary<string, (int count, DateTime timestamp)> MemoryDictionary = new();
     private readonly int _limit;
 
     public LimitPerMin(int limit = 30)
@@ -20,31 +18,33 @@ public class LimitPerMin : ActionFilterAttribute
     public override void OnActionExecuting(ActionExecutingContext context)
     {
         base.OnActionExecuting(context);
-        if (DateTime.UtcNow - _lastClearTime > TimeSpan.FromMinutes(1))
-        {
-            MemoryDictionary.Clear();
-            _lastClearTime = DateTime.UtcNow;
-        }
         var path = context.HttpContext.Request.Path.ToString();
         var ip = context.HttpContext.Connection.RemoteIpAddress?.ToString() ?? string.Empty;
         var key = ip + path;
 
+        // Clean up old entries
+        foreach (var entry in MemoryDictionary.Where(x => DateTime.UtcNow - x.Value.timestamp > TimeSpan.FromMinutes(1)).ToList())
+        {
+            MemoryDictionary.TryRemove(entry.Key, out _);
+        }
+
         if (MemoryDictionary.TryGetValue(key, out var value))
         {
-            MemoryDictionary[key] = ++value;
+            MemoryDictionary[key] = (++value.count, DateTime.UtcNow);
         }
         else
         {
-            MemoryDictionary.TryAdd(key, 1);
+            MemoryDictionary.TryAdd(key, (1, DateTime.UtcNow));
         }
 
-        if (MemoryDictionary[key] >= _limit)
+        // Check limit
+        if (MemoryDictionary.TryGetValue(key, out value) && value.count >= _limit)
         {
             context.Result = new StatusCodeResult((int)HttpStatusCode.TooManyRequests);
             return;
         }
 
         context.HttpContext.Response.Headers.Add("x-rate-limit-limit", "1m");
-        context.HttpContext.Response.Headers.Add("x-rate-limit-remaining", (_limit - MemoryDictionary[key]).ToString());
+        context.HttpContext.Response.Headers.Add("x-rate-limit-remaining", (_limit - (MemoryDictionary.TryGetValue(key, out value) ? value.count : 0)).ToString());
     }
 }
