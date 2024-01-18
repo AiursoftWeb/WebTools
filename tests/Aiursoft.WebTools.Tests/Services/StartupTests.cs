4b8b7c191b07b6f69f4d2494a0b9a1003b19721d
AnduinXue <anduin@aiursoft.com>
2024-01-18 03:41:01 +0000

    Add an integration tests.

---
+++ tests/Aiursoft.WebTools.Tests/Services/StartupTests.cs
@@ -0,0 +1,58 @@
+﻿using Aiursoft.CSTools.Tools;
+using Aiursoft.WebTools.Abstractions.Models;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Aiursoft.WebTools.Tests.Services;
+
+[TestClass]
+public class StartupTests
+{
+    [TestMethod]
+    public async Task TestHelloWorld()
+    {
+        var port = Network.GetAvailablePort();
+        var app = Extends.App<TestStartup>(Array.Empty<string>(), port, builder =>
+        {
+            builder.WebHost.ConfigureKestrel(options =>
+            {
+                // Remove the upload limit from Kestrel. If needed, an upload limit can
+                // be enforced by a reverse proxy server, like IIS.
+                options.Limits.MaxRequestBodySize = null;
+            });
+        });
+        
+        await app.StartAsync();
+        var client = new HttpClient();
+        var response = await client.GetAsync($"http://localhost:{port}/");
+        Assert.AreEqual("Hello World!", await response.Content.ReadAsStringAsync());
+    }
+}
+
+internal class TestStartup : IWebStartup
+{
+    public void ConfigureServices(IConfiguration configuration, IWebHostEnvironment environment, IServiceCollection services)
+    {
+    }
+
+    public void Configure(WebApplication app)
+    {
+        app.UseWebSockets();
+        // Hello world data:
+        app.Use(async (context, next) =>
+        {
+            if (context.Request.Path == "/")
+            {
+                await context.Response.WriteAsync("Hello World!");
+            }
+            else
+            {
+                await next();
+            }
+        });
+    }
+}
