db8dc51d11f457bc0781f2c123736eb9c67e40b7
xuef <xuef@microsoft.com>
2024-02-21 08:39:12 +0000

    Add EnforceWebSocket attribute and update AiurNoCache
    A new attribute 'EnforceWebSocket' is added to ensure requests are WebSocket. If a request isn't a WebSocket request, a status code of 400 is returned. The 'AiurNoCache' attribute is also updated to use the 'Append' method instead of 'Add' for the 'Cache-Control' and 'Expires' headers.

---
+++ src/Aiursoft.WebTools/Attributes/EnforceWebSocket.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace Aiursoft.WebTools.Attributes;
+
+public class EnforceWebSocket : ActionFilterAttribute
+{
+    public override void OnActionExecuting(ActionExecutingContext context)
+    {
+        base.OnActionExecuting(context);
+        if (!context.HttpContext.WebSockets.IsWebSocketRequest)
+        {
+            context.Result = new StatusCodeResult(400);
+        }
+    }
+}
