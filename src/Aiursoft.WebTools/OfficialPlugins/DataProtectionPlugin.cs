06bd011b8e035b44e5789c1906b535ce8a4e8b37
Anduin Xue <anduin@aiursoft.com>
2024-10-21 13:05:45 +0000

    Update DataProtectionPlugin and project version
    Removed redundant check for existing IDataProtectionProvider service and streamlined key persistence path logic. Adjusted keys path to use Environment.GetFolderPath for better flexibility. Updated project version from 8.0.25 to 8.0.26.

--- src/Aiursoft.WebTools/OfficialPlugins/DataProtectionPlugin.cs
+++ src/Aiursoft.WebTools/OfficialPlugins/DataProtectionPlugin.cs
@@ -21,26 +21,23 @@ public class DataProtectionPlugin : IWebAppPlugin
 
     public Task PostServiceConfigure(WebApplicationBuilder builder)
     {
-        var addedDataProtection = builder.Services.Any(t => t.ServiceType == typeof(IDataProtectionProvider));
-        if (!addedDataProtection)
+        // By default, this plugin uses /data/Keys as the keys path.
+        // To override the behavior, simply register the IDataProtectionProvider service in your app. (This plugin will not override it.)
+        var inDocker = EntryExtends.IsInDocker();
+        var keysPath = inDocker ? 
+            Path.Combine("/data", ".aspnet", "DataProtection-Keys") :
+            Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile), ".aspnet", "DataProtection-Keys");
+        if (!Directory.Exists(keysPath))
         {
-            // By default, this plugin uses /data/Keys as the keys path.
-            // To override the behavior, simply register the IDataProtectionProvider service in your app. (This plugin will not override it.)
-            var inDocker = EntryExtends.IsInDocker();
-            var keysPath = inDocker ? 
-                Path.Combine("/data", ".aspnet", "DataProtection-Keys") :
-                Path.Combine(Directory.GetCurrentDirectory(), "data", ".aspnet", "DataProtection-Keys");
-            if (!Directory.Exists(keysPath))
-            {
-                Directory.CreateDirectory(keysPath);
-            }
-            
-            var applicationName = Assembly.GetEntryAssembly()!.GetName().Name!;
-            Console.WriteLine($"Created directory: {keysPath} to persist data protection keys for application: {applicationName}.");
-            builder.Services.AddDataProtection()
-                .PersistKeysToFileSystem(new DirectoryInfo(keysPath))
-                .SetApplicationName(applicationName);
+            Directory.CreateDirectory(keysPath);
         }
+        
+        var applicationName = Assembly.GetEntryAssembly()!.GetName().Name!;
+        Console.WriteLine($"Created directory: {keysPath} to persist data protection keys for application: {applicationName}.");
+        builder.Services.AddDataProtection()
+            .PersistKeysToFileSystem(new DirectoryInfo(keysPath))
+            .SetApplicationName(applicationName);
+        Console.WriteLine($"Data protection plugin has been added. Keys path: {keysPath}, Application name: {applicationName}.");
         return Task.CompletedTask;
     }
 
